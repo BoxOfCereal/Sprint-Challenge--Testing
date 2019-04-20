0cfb4691ecc6cf88071689481f454cc7be07f8b3
unknown <noah.stoeckel@gmail.com>
2019-04-20 12:13:11 -0400

    sets up the games route

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const server = express();
 server.use(express.json());
+server.use("/games", require("../games/games"));
 
 server.get("/", async (req, res) => {
   res.status(200).json({ message: " the server is up" });

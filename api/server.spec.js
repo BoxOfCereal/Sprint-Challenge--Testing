d91dd131693e93db097f321384e4ac8f023fdf95
unknown <noah.stoeckel@gmail.com>
2019-04-20 12:26:44 -0400

    test server

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,13 @@
-const express = require("express");
-const router = express.Router();
+const req = require("supertest");
+const server = require("../games/games");
 
-router.get("/", (req, res) => {});
-
-module.exports = router;
+describe("the server", () => {
+  describe("GET /", () => {
+    it(" should return up a response for the message", async () => {
+      const res = await req(server).get("/");
+      expect(res.status).toBe(200);
+      expect(res.type).toBe("application/json");
+      expect(res.body).toEqual({ message: " the server is up" });
+    });
+  });
+});
